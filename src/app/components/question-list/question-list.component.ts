f20a16f7ddc0411db69d30c6153f116aedee97d4
darshan4694 <darshan.shah4694@gmail.com>
2017-08-23 12:38:40 -0500

    Toggle answer block in question list

--- src/app/components/question-list/question-list.component.ts
+++ src/app/components/question-list/question-list.component.ts
@@ -10,15 +10,18 @@ export class QuestionListComponent implements OnInit {
   questions:Object[] = [
     {
       text: 'What is your name?',
-      answer:'My name is Darshan.'
+      answer:'My name is Darshan.',
+      hide: true
     },
     {
       text: 'What is your school name?',
-      answer:'It is Southern Methodist University.'
+      answer:'It is Southern Methodist University.',
+      hide: true
     },
     {
       text: 'What is your Degree?',
-      answer:'It is MS in Software Engineering.'
+      answer:'It is MS in Software Engineering.',
+      hide: true
     },
   ];
 
