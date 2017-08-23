b8f1ca6d2e75d8f498eacb3b685d5056459abbe1
darshan4694 <darshan.shah4694@gmail.com>
2017-08-23 12:35:44 -0500

    Added questions and shown using bootstrap 'card'

--- src/app/components/question-list/question-list.component.ts
+++ src/app/components/question-list/question-list.component.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionListComponent implements OnInit {
 
+  questions:Object[] = [
+    {
+      text: 'What is your name?',
+      answer:'My name is Darshan.'
+    },
+    {
+      text: 'What is your school name?',
+      answer:'It is Southern Methodist University.'
+    },
+    {
+      text: 'What is your Degree?',
+      answer:'It is MS in Software Engineering.'
+    },
+  ];
+
   constructor() { }
 
   ngOnInit() {
